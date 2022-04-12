a710a608ba627e08d16c453d7d86c4634de6baba
Sonali Singh <singh.sonal@northeastern.edu>
2022-04-12 01:28:44 -0400

    Lambda Changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-export const handler  =async(event, context, callback) =>{
-    console.log('Sonali I am here!!');
+exports.handler = function(event, context, callback) {
+    // console.log('Received event:', JSON.stringify(event, null, 4));
+    
         var message = event.Records[0].Sns.Message;
+        var token = event.Records[0].Sns.Subject;
         console.log('Message received from SNS:', message);
+        console.log('Message received from SNS:', token);
         callback(null, "Success");
-    }
+    };
