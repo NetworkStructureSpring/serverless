5339cfcf1fc8e8f8cc5630e73aacaa84a0bb405c
Sonali Singh <singh.sonal@northeastern.edu>
2022-04-12 12:59:06 -0400

    Code changes testing

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-exports.handler = function(event, context, callback) {
-    // console.log('Received event:', JSON.stringify(event, null, 4));
+const AWS = require('aws-sdk');
+exports.handler = function (event, context, callback) {
     
         var message = event.Records[0].Sns.Message;
         var token = event.Records[0].Sns.Subject;
         console.log('Message received from SNS:', message);
         console.log('Message received from SNS:', token);
         callback(null, "Success");
-    };
+        const SES_CONFIG = {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+            region: 'us-east-1',
+        };
+
+        const AWS_SES = new AWS.SES(SES_CONFIG);
+
+        let sendEmail = (recipientEmail, name) => {
+            let params = {
+            Source: 'sonali@prod.sonalisingh30.me',
+            Destination: {
+                ToAddresses: [
+                    message
+                ],
+            },
+            ReplyToAddresses: [],
+            Message: {
+                Body: {
+                Html: {
+                    Charset: 'UTF-8',
+                    Data: 'This is the body of my email!',
+                },
+                },
+                Subject: {
+                Charset: 'UTF-8',
+                Data: `Hello, ${message}!`,
+                }
+            },
+            };
+            return AWS_SES.sendEmail(params).promise();
+        };    
+};
+module.exports = {
+    sendEmail,
+    sendTemplateEmail,
+  };
